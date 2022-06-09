36b59843bd2f1418b0525068fa4efdd6fc2aa403
benguang.zhao <benguang.zhao@gmail.com>
2022-06-09 18:11:18 +0800

    Update tools/showfn.sh to just search C source code

--- tools/showfn.sh
+++ tools/showfn.sh
@@ -1,1 +1,4 @@
-grep -oHE ' [a-z][a-z\_A-Z0-9]+\(' -R . | awk -F': ' '{print $2}' | sed 's/(//g' | sort --uniq | grep -v -E 'str.+|pthread|free|.*alloc|mem.+|.*[Pp]rint|usleep'
+#!/bin/bash
+
+find . -type f -a -name '*.c' -o -name '*.h' |\
+	xargs -I {} grep -oHE ' [a-z][a-z\_A-Z0-9]{5,}\(' {} | awk -F': ' '{print $2}' | sed 's/(//g' | sort --uniq | grep -v -E 'str.+|pthread|free|.*alloc|mem.+|.*[Pp]rint|usleep'
